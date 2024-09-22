3f1e5903419820a3eedf1bc009c2888892ad9384
Anıl Başaran <nlbsrn@gmail.com>
2024-09-22 04:08:17 +0300

    added some breaking points for styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="p-8 font-tiny">
+    <div className="p-8 font-tiny md:bg-slate-300 lg:bg-blue-500">
       <div className="text-xl">this is the home page</div>
       <Link className="my-10 text-blue-400 underline" to="/other">
         go to other page
